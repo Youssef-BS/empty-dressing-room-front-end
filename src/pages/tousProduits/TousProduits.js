7568071740a6db945524ce218104026029bc3436
Djosef0 <youssefbensaid827@gmail.com>
2023-01-30 18:05:14 +0100

    add function to fetch all data produits

--- src/pages/tousProduits/TousProduits.js
+++ src/pages/tousProduits/TousProduits.js
@@ -1,9 +1,38 @@
-import React from 'react';
+import React,{useState , useEffect} from 'react';
 import "./tousProduits.css";
+import axios from "axios";
 
 const TousProduits = () => {
+
+  const [produits , setProoduits] = useState([]);
+
+  useEffect(()=>{
+   
+    const fetchData = async () =>{
+      try{
+       const res = await axios.get("http://localhost:4000/api/produits");
+       setProoduits(res.data)
+      }catch(error){
+        console.log(error)
+      }
+      
+    } 
+
+    fetchData();
+   
+  })
+  
+// console.log(produits[0].desc)
   return (
-    <div>TousProduits</div>
+    <>
+    {produits.map(item=>(
+      
+        <div className='contain' key={item._id}>
+          <p>{item.desc}</p>
+  </div>
+    
+    ))}
+    </>
   )
 }
 
