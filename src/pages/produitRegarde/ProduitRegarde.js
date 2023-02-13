6d2f670d5798327cdf7b2f5d3d74dc7f4a68241e
Djosef0 <youssefbensaid827@gmail.com>
2023-02-13 12:15:33 +0100

    add form modal for message

--- src/pages/produitRegarde/ProduitRegarde.js
+++ src/pages/produitRegarde/ProduitRegarde.js
@@ -2,10 +2,18 @@ import React,{useEffect , useState} from "react"
 import axios from 'axios';
 import {useParams} from 'react-router-dom'
 import './produitRegarde.css'
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import Form from 'react-bootstrap/Form';
 
 const ProduitRegarde = () =>{
 const params = useParams()
 const [productSlect , setProductSelect] = useState({})
+
+const [show, setShow] = useState(false);
+
+const handleClose = () => setShow(false);
+const handleShow = () => setShow(true);
 
 useEffect(()=>{
 const fetchData =async ()=>{
@@ -30,14 +38,41 @@ console.log(productSlect)
          <div className="foto"><img src={productSlect.product.photoProduit.url}  alt="" /></div>
          <div className="statistic">
         <p>{productSlect.product.title}</p>
-        <p>{productSlect.product.taille}</p>
-        <p>{productSlect.product.marque}</p>
-        <p>{productSlect.product.categorie}</p>
-        <p><b>{productSlect.product.price}</b></p>
+        <p>{productSlect.product.desc}</p>
+        <p>Taille : {productSlect.product.taille}</p>
+        <p>Marque : {productSlect.product.marque}</p>
+        <p><b>Prix : {productSlect.product.price} Dt</b></p>
+        <button>Acheter</button>
+        <button onClick={handleShow} >Cantacter le Vendeur</button>
         </div>
         </div>
         )}
-        
+  
+  <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Messagerie</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+          <Form.Control as="textarea" rows={7} disabled/>
+            <Form.Group
+              className="mb-3"
+              controlId="exampleForm.ControlTextarea1"
+            >
+              <Form.Label>Ecrire message ici</Form.Label>
+              <Form.Control as="textarea" rows={1} />
+            </Form.Group>
+            <button variant="secondary" onClick={handleClose}>
+            Close
+          </button>
+          <input type='button' variant="primary" className="BtnForm"  value="envoyer" />
+             
+          </Form>
+ 
+        </Modal.Body>
+      
+      </Modal>
+         
         </>
     )
 
